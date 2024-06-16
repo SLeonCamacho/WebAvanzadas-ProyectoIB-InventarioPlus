5f2c8094493ad21161a2a126a30d77f5b4669c60
Santiago León <sleoncamacho@outlook.com>
2024-06-16 08:57:45 -0500

    Created project structure
    Used App Router to create the basic navigation of the page, soon to be coded

---
+++ inventarioplus/src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ inventarioplus/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>This is a login page</h1>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default Login;
