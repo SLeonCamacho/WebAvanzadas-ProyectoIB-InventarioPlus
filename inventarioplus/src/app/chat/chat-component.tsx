24fed3f8c117f6fd9dc419b7a37d383d8c64baea
Santiago León <sleoncamacho@outlook.com>
2024-06-16 14:42:52 -0500

    Fixed the color of texts in dashboard
    Some texts where white on white. The chat X button was also changed to a grey color.

--- inventarioplus/src/app/chat/chat-component.tsx
+++ inventarioplus/src/app/chat/chat-component.tsx
@@ -9,7 +9,7 @@ const Chat: React.FC<ChatProps> = ({ onClose }) => {
     <div className="fixed bottom-0 right-0 w-150 p-4 bg-white rounded-t-lg shadow-lg">
       <div className="flex justify-between items-center mb-4">
         <p className="text-gray-700">Chat messages will appear here.</p>
-        <button onClick={onClose} className="text-red-500 hover:text-red-700">X</button>
+        <button onClick={onClose} className="text-gray-700 hover:text-red-700">X</button>
       </div>
       <div className="h-60 overflow-y-scroll border-b-2 border-gray-200 p-2 mb-4">
         {/* Here you will loop over and display chat messages */}
@@ -18,7 +18,7 @@ const Chat: React.FC<ChatProps> = ({ onClose }) => {
         <input 
           type="text" 
           placeholder="Type a message..." 
-          className="flex-1 px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="flex-1 px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
         />
         <button 
           type="submit" 
--- inventarioplus/src/app/dashboard/page.tsx
+++ inventarioplus/src/app/dashboard/page.tsx
@@ -18,9 +18,9 @@ const Dashboard = () => {
   return (
     <section className="relative flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
       <div className="text-center mb-8">
-        <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+        <h1 className="text-3xl font-bold mb-4 text-black">Dashboard</h1>
         <p className="text-lg text-gray-700 mb-4">A dashboard to display data and perform CRUD operations.</p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black">
           <div className="p-4 bg-white rounded shadow-md">
             <h2 className="text-xl font-semibold mb-2">Data Section 1</h2>
             <p>Details about this section.</p>
@@ -31,6 +31,10 @@ const Dashboard = () => {
           </div>
           <div className="p-4 bg-white rounded shadow-md">
             <h2 className="text-xl font-semibold mb-2">Data Section 3</h2>
+            <p>Details about this section.</p>
+          </div>
+          <div className="p-4 bg-white rounded shadow-md">
+            <h2 className="text-xl font-semibold mb-2">Data Section 4</h2>
             <p>Details about this section.</p>
           </div>
         </div>
