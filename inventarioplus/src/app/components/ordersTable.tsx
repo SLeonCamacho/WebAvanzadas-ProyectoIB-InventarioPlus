d8cf79abef26c94fe3e0676554622d79905cb539
Santiago León <sleoncamacho@outlook.com>
2024-06-18 11:14:12 -0500

    Added keys ids again
    Now it work in dev env with keys in place. Let's see if it work in build.

--- inventarioplus/src/app/components/ordersTable.tsx
+++ inventarioplus/src/app/components/ordersTable.tsx
@@ -13,13 +13,12 @@ const OrdersTable = ({ data }: { data: Orders[] }) => {
       {
         Header: 'User ID',
         accessor: 'user_id',
-        align: 'center',
+        align: 'left',
       },
       {
         Header: 'Order Date',
-        accessor: 'order_date',
-        align: 'center',
-        Cell: ({ value }) => new Date(value).toLocaleDateString(),
+        accessor: (row) => new Date(row.order_date).toLocaleDateString(),
+        align: 'left',
       },
       {
         Header: 'Total',
@@ -43,12 +42,13 @@ const OrdersTable = ({ data }: { data: Orders[] }) => {
       <table {...getTableProps()} className="min-w-full bg-white border border-blue-200">
         <thead className="bg-blue-50">
           {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
+            <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
               {headerGroup.headers.map(column => (
                 <th
                   {...column.getHeaderProps()}
                   className="px-6 py-3 border-b border-blue-200 text-black text-left text-sm uppercase font-medium"
                   style={{ textAlign: (column as any).align }}
+                  key={column.id}
                 >
                   {column.render('Header')}
                 </th>
@@ -60,12 +60,13 @@ const OrdersTable = ({ data }: { data: Orders[] }) => {
           {rows.map(row => {
             prepareRow(row);
             return (
-              <tr {...row.getRowProps()} className="hover:bg-blue-100">
+              <tr {...row.getRowProps()} className="hover:bg-blue-100" key={row.id}>
                 {row.cells.map(cell => (
                   <td
                     {...cell.getCellProps()}
                     className="px-6 py-4 whitespace-nowrap text-sm text-black"
                     style={{ textAlign: (cell.column as any).align }}
+                    key={cell.column.id}
                   >
                     {cell.render('Cell')}
                   </td>
