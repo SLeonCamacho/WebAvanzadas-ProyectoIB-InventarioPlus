1ccf835302068ad26d2013cdf558f766ca63a1e2
Santiago León <sleoncamacho@outlook.com>
2024-06-18 11:41:36 -0500

    Fixed issue with useSearchParams not wrapped
    The use of useSearchParams should have been wrapped in a Suspense boundry.

---
+++ inventarioplus/src/app/renewPass/RenewPassContent.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import { updateUserPass } from '../api/update-data/async-queries-user';
+
+const RenewPassContent = () => {
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const emailParam = searchParams.get('email');
+    if (emailParam) {
+      setEmail(decodeURIComponent(emailParam));
+    }
+  }, [searchParams]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setError('Passwords do not match');
+    } else {
+      setError('');
+      try {
+        const userPassUpdated = await updateUserPass(email, newPassword);
+        if (userPassUpdated.length > 0) {
+          router.push('/renewPass/passwordResetSuccess');
+        }
+      } catch (error) {
+        console.log('Error updating password:', error);
+      }
+    }
+  };
+
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <div className="text-center mb-8">
+        <h1 className="text-3xl font-bold mb-4 text-black">Reset Password</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+          <input 
+            type="password" 
+            placeholder="New Password" 
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            className="px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+            required
+          />
+          <input 
+            type="password" 
+            placeholder="Confirm Password" 
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+            required
+          />
+          {error && <p className="text-red-500">{error}</p>}
+          <button 
+            type="submit" 
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Reset Password
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default RenewPassContent;
---
+++ inventarioplus/src/app/signUp/success/SignUpSuccessContent.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+const SignUpSuccessContent = () => {
+  const [name, setName] = useState('');
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const nameParam = searchParams.get('name');
+    if (nameParam) {
+      setName(decodeURIComponent(nameParam));
+    }
+  }, [searchParams]);
+
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <div className="text-center mb-8">
+        <h1 className="text-3xl font-bold mb-4 text-black">All set up!</h1>
+        {name && <p className="text-black">We are happy to have you with us, {name}</p>}
+        <br />
+        <Link className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" href="/login">
+          Back to Login
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default SignUpSuccessContent;
